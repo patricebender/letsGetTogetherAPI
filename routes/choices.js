10e4544964735a044599e4d66860496c069abc2c
Patrice Bender <info@patrice.codes>
2018-12-19 17:39:19 +0100

    Add first card for future category 'choices'

---
+++ routes/choices.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        category: "choice",
+        question: 'Hast du eine Nintendo Switch',
+        options: [
+            {
+                "title": "Ja",
+                "loser": "others",
+                "sips": 3
+            },
+            {
+                "title": "Nein",
+                "loser": "player",
+                "sips": 3
+            }
+        ]
+    }
+]
